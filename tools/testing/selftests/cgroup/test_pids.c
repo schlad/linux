3f591f39b234b49e801cff93742429bb2a6b13e7
Sebastian Chlad <sebastian.chlad@suse.com>
2025-07-31 22:15:12 +0200

    s

--- tools/testing/selftests/cgroup/test_pids.c
+++ tools/testing/selftests/cgroup/test_pids.c
@@ -129,6 +129,10 @@ static int test_pids_events(const char *root)
     }
 
     long child_max = cg_read_key_long(cg_child, "pids.events", "max ");
+	long parent_max = cg_read_key_long(cg_parent, "pids.events", "max ");
+
+	printf("DEBUG: child pids.events max = %ld\n", child_max);
+	printf("DEBUG: parent pids.events max = %ld\n", parent_max);
     if (child_max != 0) {
         printf("Unexpected child pids.events 'max' count: %ld (expected 0)\n", child_max);
         goto cleanup;
